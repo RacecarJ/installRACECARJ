36081b2c525da0b2479dfb9b7b2e307c5472badd
RacecarJ <jim@racecarj.com>
2019-08-19 10:15:42 -0700

    Add VESC6 support

---
+++ installMITRACECARVESC6.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Install the MIT RACECAR software
+# This is for NVIDIA Jetson L4T 28.1
+# This will install the whole software stack for the MIT RACECAR
+# Includes ROS installation
+
+# L4T 28.1 does not have a cdc-acm driver
+./scripts/installCDCACM.sh
+# Setup the UDEV rules for the ESC and IMU
+./scripts/installRACECARUdev.sh
+# Install ROS
+./scripts/installROS.sh
+# Then setup the Catkin Workpsace for the RACECAR
+./scripts/setupCatkinWorkspace.sh racecar-ws
+# One of the dependencies is missing, opencv3 for ros
+sudo apt-get install ros-kinetic-opencv3 -y
+# And now install the MIT RACECAR packages
+./scripts/installMITRACECARVESC6.sh racecar-ws
+# Print out a little info at the end
+./scripts/echoInfo.sh
+echo "The RACECAR Packages should now be installed in the directory racecar-ws"
+echo " " 
+
+
+
---
+++ scripts/installMITRACECARVESC6.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Setup the github/mit-racecar/ ROS structure
+# Usage installRACECAR.sh <dirName>
+# dirName defaults to racecar-ws
+# Fetches RACECAR ROS components, installs dependencies, and then catkin_make the workspace
+
+source /opt/ros/kinetic/setup.bash
+DEFAULTDIR=~/racecar-ws
+CLDIR="$1"
+if [ ! -z "$CLDIR" ]; then 
+ DEFAULTDIR=~/"$CLDIR"
+fi
+if [ -e "$DEFAULTDIR" ] ; then
+  echo "$DEFAULTDIR already exists; no action taken"
+  echo "Placing RACECAR code into $DEFAULTDIR" 
+else 
+  echo "$DEFAULTDIR does not exist; no action taken."
+  echo "This script only uses an existing initialized Catkin Workspace for the RACECAR code."
+  exit 1
+fi
+cd "$DEFAULTDIR"
+
+wget -q https://raw.githubusercontent.com/racecarj/racecar/VESC6/racecar.rosinstall -O "$DEFAULTDIR"/.rosinstall
+
+wstool update
+
+# Install prerequisite packages
+echo "Installing prerequisites"
+source devel/setup.bash
+# Install the rosdeps -a = all -y = no questions -r = skip errors (for openCV in ZED_Wrapper)
+rosdep install -a -y -r
+# jstest-gtk is added for testing the joystick
+sudo apt-get -y install \
+    jstest-gtk 
+
+echo "Catkin Make"
+# On the Jetson, there's currently an issue with using the dynamic runtime
+# Typically this reports as "cannot find -lopencv_dep_cudart" in the error log
+catkin_make config --cmake-args -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
+catkin_make
+
